e673265142a73b0919ce63dd66882a92670f7aa9
Alex Suraci <suraci.alex@gmail.com>
2014-05-17 10:27:09 -0700

    update for new prole build api

--- Godeps/_workspace/src/github.com/winston-ci/prole/api/builds/build.go
+++ Godeps/_workspace/src/github.com/winston-ci/prole/api/builds/build.go
@@ -1,28 +1,34 @@
 package builds
 
+import "encoding/json"
+
 type Build struct {
 	Guid string `json:"guid"`
 
-	ConfigPath string `json:"config"`
-
-	Image  string      `json:"image"`
-	Env    [][2]string `json:"env"`
-	Script string      `json:"script"`
-
-	Privileged bool `json:"privileged"`
+	Config Config `json:"config"`
 
 	LogsURL  string `json:"logs_url"`
 	Callback string `json:"callback"`
 
-	Sources []BuildSource `json:"sources"`
+	Inputs []Input `json:"inputs"`
 
 	Status string `json:"status"`
 }
 
-type BuildSource struct {
-	Type   string `json:"type"`
-	URI    string `json:"uri"`
-	Branch string `json:"branch"`
-	Ref    string `json:"ref"`
-	Path   string `json:"path"`
+type Config struct {
+	Image string `json:"image"`
+
+	Env    [][2]string `json:"env"`
+	Script string      `json:"script"`
+
+	Privileged bool `json:"privileged"`
+}
+
+type Input struct {
+	Type string `json:"type"`
+
+	ConfigPath      string `json:"configPath"`
+	DestinationPath string `json:"destinationPath"`
+
+	Version *json.RawMessage `json:"version"`
 }
