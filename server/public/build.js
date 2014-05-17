dce24c6cf77df0368aaf9799cdb042d71f431257
Alex Suraci <suraci.alex@gmail.com>
2014-05-17 12:04:21 -0700

    auto-scroll with terminal-like behavior
    when the user hits the bottom of the page, keep following. if they scroll up,
    stop following. follow by default.

---
+++ server/public/build.js
@@ -0,0 +1,27 @@
+var autoscroll = true;
+
+function streamLog(uri) {
+  var ws = new WebSocket(uri);
+
+  ws.onmessage = function(event) {
+    document.getElementById("build-log").innerHTML += event.data
+
+		if (autoscroll) {
+			$(document).scrollTop($(document).height());
+		}
+  };
+}
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+	  var scrollEnd = $(window).scrollTop() + $(window).height();
+
+	  if (scrollEnd == $(document).height()) {
+	    autoscroll = true;
+	  } else {
+	    autoscroll = false;
+    }
+
+    scrolled = true;
+	});
+});
