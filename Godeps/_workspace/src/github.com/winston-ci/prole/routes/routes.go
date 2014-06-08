b8721489a2d437dae55786a82951f471a2fca812
Alex Suraci <suraci.alex@gmail.com>
2014-06-08 12:36:22 -0700

    deps

--- Godeps/_workspace/src/github.com/winston-ci/prole/routes/routes.go
+++ Godeps/_workspace/src/github.com/winston-ci/prole/routes/routes.go
@@ -3,13 +3,15 @@ package routes
 import "github.com/tedsuo/router"
 
 const (
-	ExecuteBuild = "ExecuteBuild"
-	AbortBuild   = "AbortBuild"
-	CheckInput   = "CheckInput"
+	ExecuteBuild     = "ExecuteBuild"
+	AbortBuild       = "AbortBuild"
+	CheckInput       = "CheckInput"
+	CheckInputStream = "CheckInputStream"
 )
 
 var Routes = router.Routes{
 	{Path: "/builds", Method: "POST", Handler: ExecuteBuild},
 	{Path: "/builds/:guid/abort", Method: "POST", Handler: AbortBuild},
 	{Path: "/checks", Method: "POST", Handler: CheckInput},
+	{Path: "/checks/stream", Method: "GET", Handler: CheckInputStream},
 }
