1ad87c1ff62ea7277dcdae73282f3acc6c0e2ef2
Alex Suraci <suraci.alex@gmail.com>
2017-10-24 12:22:07 -0400

    fix switching to/from initially viewed build

--- web/wats/test/build.js
+++ web/wats/test/build.js
@@ -18,6 +18,27 @@ test.always.afterEach(async t => {
   await t.context.finish(t);
 });
 
+test('can be switched between', async t => {
+  await t.context.fly.run('set-pipeline -n -p some-pipeline -c fixtures/states-pipeline.yml');
+  await t.context.fly.run('unpause-pipeline -p some-pipeline');
+
+  await t.context.fly.run('trigger-job -w -j some-pipeline/passing');
+  await t.context.fly.run('trigger-job -w -j some-pipeline/passing');
+
+  await t.context.page.goto(t.context.web.route(`/teams/${t.context.teamName}/pipelines/some-pipeline/jobs/passing/builds/1`));
+
+  await t.context.page.waitFor('#builds li:nth-child(1) a');
+  await t.context.page.waitFor('#builds li:nth-child(2) a');
+
+  await t.context.page.click('#builds li:nth-child(1) a');
+  await t.context.page.waitForNavigation({waitUntil: 'networkidle'});
+  t.regex(await t.context.web.text(t.context.page), /passing #2/);
+
+  await t.context.page.click('#builds li:nth-child(2) a');
+  await t.context.page.waitForNavigation({waitUntil: 'networkidle'});
+  t.regex(await t.context.web.text(t.context.page), /passing #1/);
+});
+
 test('scrolls to the top with gg, and to the bottom with G', async t => {
   await t.context.fly.run('set-pipeline -n -p some-pipeline -c fixtures/pipeline-with-long-output.yml');
   await t.context.fly.run('unpause-pipeline -p some-pipeline');
