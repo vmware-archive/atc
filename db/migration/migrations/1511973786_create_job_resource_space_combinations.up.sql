d39dceae7e6199f67ae24b2889b70cdda279ffb8
Mark Huang <mhuang@pivotal.io>
2017-12-06 17:53:24 -0500

    Add migration to create job_resource_space_combinations table
    Signed-off-by: Rui Yang <ryang@pivotal.io>

---
+++ db/migration/migrations/1511973786_create_job_resource_space_combinations.up.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+  CREATE TABLE job_resource_space_combinations (
+    job_id int REFERENCES jobs (id) ON DELETE CASCADE,
+    resource_space_id int REFERENCES resource_spaces (id) ON DELETE CASCADE,
+    combination jsonb NOT NULL,
+    hash varchar(20) NOT NULL,
+    UNIQUE (job_id, resource_space_id, hash)
+  );
+
+COMMIT;
