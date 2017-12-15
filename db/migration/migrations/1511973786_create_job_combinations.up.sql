46c8abe545a582697519f172b3f43409648ee0a6
Mark Huang <mhuang@pivotal.io>
2017-12-15 11:44:24 -0500

    WIP
    Migrate jobs to job_combinations and job_combinations_resource_spaces
    
    TODO: determine which resource should be saved in job_combinations_resource_spaces when the same resource is duplicated
    
    Signed-off-by: Joris Melchior <jmelchior@pivotal.io>

---
+++ db/migration/migrations/1511973786_create_job_combinations.up.sql
@@ -0,0 +1,35 @@
+BEGIN;
+
+  CREATE TABLE job_combinations (
+    id SERIAL PRIMARY KEY,
+    job_id int REFERENCES jobs (id) ON DELETE CASCADE,
+    combination jsonb,
+    inputs_determined boolean DEFAULT false
+  );
+
+  INSERT INTO job_combinations(id, job_id, combination)
+  SELECT id, id, json_object_agg(resource_name, 'default')
+  FROM jobs, LATERAL (SELECT DISTINCT(json_array_elements(config::json->'plan')->>'get') AS resource_name) _
+  WHERE resource_name is NOT NULL
+  GROUP BY id;
+
+  INSERT INTO job_combinations(id, job_id, combination)
+  SELECT id, id, json_object_agg(resource_name, 'default')
+  FROM jobs, LATERAL (SELECT DISTINCT(json_array_elements(config::json->'plan')->>'put') AS resource_name) _
+  WHERE resource_name is NOT NULL
+  GROUP BY id;
+
+  /* SELECT id, id, json_object_agg(resource_name, 'default') */
+  /* FROM jobs, LATERAL (SELECT (json_each_text(json_array_elements((config::json->'plan')))).value AS resource_name) _ GROUP BY id; */
+
+  SELECT setval('job_combinations_id_seq', (SELECT max(id) FROM job_combinations));
+
+  /* ALTER TABLE builds ADD COLUMN job_combination_id varchar(20); */
+  /* ALTER TABLE builds ADD CONSTRAINT job_combination_id_fkey FOREIGN KEY (job_combination_id) REFERENCES job_resource_space_combinations (hash) ON DELETE CASCADE; */
+  /* CREATE INDEX builds_job_id_ ON builds USING btree (interceptible, completed); */
+
+  /* ALTER TABLE next_build_inputs ADD COLUMN job_combination_id varchar(20); */
+  /* ALTER TABLE independent_build_inputs ADD COLUMN job_combination_id varchar(20) */
+  /* ALTER TABLE worker_task_caches ADD COLUMN job_combination_id varchar(20) */
+
+COMMIT;
