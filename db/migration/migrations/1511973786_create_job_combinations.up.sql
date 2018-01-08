ee04a10bf0e455b62f6fea1b1c0e6301ff27a6a7
Mark Huang <mhuang@pivotal.io>
2018-01-08 11:10:01 -0500

    Rename job_id to job_combination_id in the builds table
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

--- db/migration/migrations/1511973786_create_job_combinations.up.sql
+++ db/migration/migrations/1511973786_create_job_combinations.up.sql
@@ -9,12 +9,9 @@ BEGIN;
 
   CREATE UNIQUE INDEX job_combinations_job_id_combination_key ON job_combinations (job_id, combination);
 
-  /* ALTER TABLE builds ADD COLUMN job_combination_id varchar(20); */
-  /* ALTER TABLE builds ADD CONSTRAINT job_combination_id_fkey FOREIGN KEY (job_combination_id) REFERENCES job_resource_space_combinations (hash) ON DELETE CASCADE; */
-  /* CREATE INDEX builds_job_id_ ON builds USING btree (interceptible, completed); */
-
-  /* ALTER TABLE next_build_inputs ADD COLUMN job_combination_id varchar(20); */
-  /* ALTER TABLE independent_build_inputs ADD COLUMN job_combination_id varchar(20) */
-  /* ALTER TABLE worker_task_caches ADD COLUMN job_combination_id varchar(20) */
+  ALTER TABLE builds RENAME job_id TO job_combination_id;
+  ALTER TABLE builds DROP CONSTRAINT fkey_job_id;
+  ALTER TABLE builds ADD CONSTRAINT fkey_job_combination_id FOREIGN KEY (job_combination_id) REFERENCES job_combinations(id) ON DELETE CASCADE;
+  ALTER INDEX builds_job_id RENAME TO builds_job_combination_id;
 
 COMMIT;
